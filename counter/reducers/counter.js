68637ca2fca68a2c9ee39422347754d2890721fa
laznrbfe <laznrbfe@163.com>
2016-05-23 09:33:09 +0800

    reducer

--- counter/reducers/counter.js
+++ counter/reducers/counter.js
@@ -1,1 +1,26 @@
 // counter.js
+import * as types from '../actions/actionTypes';
+
+const initialState = {
+  count: 0,
+};
+
+export default function counter(state:initialState, action) {
+  switch (action.type) {
+    case types.INCREASE:
+      return {
+        ...state,
+        count: state.count+1
+      };
+      break;
+    case types.DECREASE:
+    return {
+      ...state,
+      count: state.count-1
+    };
+      break;
+    default:
+      state,
+
+  }
+}
